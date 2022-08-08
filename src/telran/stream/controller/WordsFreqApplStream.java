758d49d7aaafd6112045b8c46d3f54a778757aba
TimurMP <titimurmamyshev@gmail.commur>
2022-08-08 23:52:32 +0300

    Added WordsFreqApplStream, formatted + final

--- src/telran/stream/controller/WordsFreqApplStream.java
+++ src/telran/stream/controller/WordsFreqApplStream.java
@@ -13,7 +13,8 @@ public class WordsFreqApplStream {
         Arrays.stream(words)
                 .collect(Collectors.groupingBy(s -> s, Collectors.counting()))
                 .entrySet()
-                .stream().sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+                .stream()
+                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
                 .forEach(System.out::println);
     }
 
